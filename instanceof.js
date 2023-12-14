ec06bd57d57c415aaa053b09a7a80be8d2fa183e
Portela <daniloportela97@gmail.com>
2023-12-14 14:52:54 -0300

    Instanceof.

---
+++ instanceof.js
@@ -0,0 +1,18 @@
+function BankAcount(personName, initialBalance, address) {
+  this.personName = personName;
+  (this.initialBalance = initialBalance), (this.address = address);
+
+  this.printInfo = function () {
+    console.log(
+      `Essa conta pertence a ${this.personName}, residente no endereço ${
+        this.address
+      } e possui um saldo atual de ${Number(this.initialBalance).toFixed(
+        2
+      )} reais.`
+    );
+  };
+}
+
+const ba1 = new BankAcount("Portela", 2000, "Rua A");
+ba1.printInfo();
+console.log(ba1 instanceof BankAcount);
