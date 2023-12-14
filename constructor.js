a45490bdfcdb60273ca97290cd7335d42595cca4
Portela <daniloportela97@gmail.com>
2023-12-14 13:41:21 -0300

    Constructor.

---
+++ constructor.js
@@ -0,0 +1,63 @@
+// Object Literal
+
+const account1 = {
+  name_: "Portela",
+  accountNumber: "12345678-9",
+  balance: 500,
+  displayBalance: function () {
+    console.log(
+      `Account number ${this.accountNumber} has total balance of ${this.balance}`
+    );
+  },
+};
+
+const account2 = {
+  name_: "Danilo",
+  accountNumber: "4345890-1",
+  balance: 50000,
+  displayBalance: function () {
+    console.log(
+      `Account number ${this.accountNumber} has total balance of ${this.balance}`
+    );
+  },
+};
+
+// console.log(account1);
+// account1.displayBalance();
+
+// Função Fábrica - Factory Function
+
+function createAccount(userName, accountNumber, initialBalance) {
+  return {
+    name_: userName,
+    accountNumber,
+    balance: initialBalance,
+    displayBalance: function () {
+      console.log(
+        `Account number ${this.accountNumber} has total balance of ${this.balance}`
+      );
+    },
+  };
+}
+
+const accountFunction = createAccount("Portela", "4345890-1", 500);
+
+// console.log(accountFunction);
+// accountFunction.displayBalance();
+
+// Construtor - Constructor
+
+function Account(userName, accountNumber, initialBalance) {
+  this.name_ = userName;
+  (this.accountNumber = accountNumber), (this.balance = initialBalance);
+  this.displayBalance = function () {
+    console.log(
+      `Account number ${this.accountNumber} has total balance of ${this.balance}`
+    );
+  };
+}
+
+const constructorAccount = new Account("Portela", "4345890-1", 500);
+
+console.log(constructorAccount);
+constructorAccount.displayBalance();
